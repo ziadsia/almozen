c8afe3719a11e510575be6320942a2b1005d7b49
FlutLab.io <>
2022-07-24 11:18:04 +0000

    Initial commit

---
+++ android/app/src/main/kotlin/com/almozen10/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.almozen10
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,66 @@
+import 'package:easy_localization/easy_localization.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart' show DeviceOrientation, SystemChrome;
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:prayertimes/screens/splash_screen.dart';
+import 'package:prayertimes/ui/helper/AppConstants.dart' show AppConstants;
+import 'package:prayertimes/ui/theme/dark_theme.dart' show themeDarkData;
+import 'package:prayertimes/ui/theme/light_theme.dart' show themeLightData;
+import 'package:prayertimes/ui/helper/local_notifications_helper.dart';
+import 'package:provider/provider.dart' show ChangeNotifierProvider, MultiProvider, Provider;
+
+import 'generated/locale_keys.g.dart';
+import 'models/custom_theme_mode.dart' show CustomThemeMode;
+import 'screens/home_page.dart' show HomePage;
+import 'screens/onboarding_page.dart' show OnboardingPage;
+
+final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
+NotificationAppLaunchDetails notificationAppLaunchDetails;
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  notificationAppLaunchDetails = await flutterLocalNotificationsPlugin.getNotificationAppLaunchDetails();
+  await initNotifications(flutterLocalNotificationsPlugin);
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp
+  ]);
+  runApp(
+    EasyLocalization(
+      supportedLocales: [
+        AppConstants.TR_LOCALE,
+        AppConstants.EN_LOCALE,
+        AppConstants.AR_LOCALE
+      ],
+      path: AppConstants.LANG_PATH,
+      child: MultiProvider(
+        providers: [
+          ChangeNotifierProvider(builder: (context) => CustomThemeMode()),
+        ],
+        child: PrayerTimes(),
+      ),
+    ),
+  );
+}
+
+class PrayerTimes extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: LocaleKeys.appName.tr(),
+      theme: themeLightData,
+      darkTheme: themeDarkData,
+      themeMode: Provider.of<CustomThemeMode>(context).getThemeMode,
+      debugShowCheckedModeBanner: false,
+      localizationsDelegates: context.localizationDelegates,
+      supportedLocales: context.supportedLocales,
+      locale: context.locale,
+      initialRoute: '/splash',
+      routes: {
+        '/splash': (context) => CustomSplashScreen(),
+        '/home': (context) => HomePage(),
+        '/onboarding': (context) => OnboardingPage(),
+      },
+    );
+  }
+}
